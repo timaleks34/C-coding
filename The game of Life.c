afe543aedd0989f9b5908a9bbbbb59bb04d8a54e
timaleks34 <145840570+timaleks34@users.noreply.github.com>
2023-10-28 09:21:07 +0300

    Update The game of Life.c

--- The game of Life.c
+++ The game of Life.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 #define MAX_ROWS 10
 #define MAX_COLS 10
 
 int countNeighbors(int board[][MAX_COLS], int row, int col);
 void displayBoard(int board[][MAX_COLS]);
 void lifeGame(int board[][MAX_COLS]);
-
 int main() {
     srand(time(0));
     int board[MAX_ROWS][MAX_COLS];
-
-    // Initialize the game board with random live or dead cells
     for (int i = 0; i < MAX_ROWS; i++) {
         for (int j = 0; j < MAX_COLS; j++) {
             board[i][j] = rand() % 2;
         }
     }
-
     lifeGame(board);
     return 0;
 }
 
 int countNeighbors(int board[][MAX_COLS], int row, int col) {
     int count = 0;
-
     for (int i = row - 1; i <= row + 1; i++) {
         for (int j = col - 1; j <= col + 1; j++) {
             if (i >= 0 && i < MAX_ROWS && j >= 0 && j < MAX_COLS) {
@@ -34,10 +28,7 @@ int countNeighbors(int board[][MAX_COLS], int row, int col) {
             }
         }
     }
-
-    // Subtract the cell itself to get the count of live neighbors
     count -= board[row][col];
-
     return count;
 }
 
@@ -52,12 +43,9 @@ void displayBoard(int board[][MAX_COLS]) {
 
 void lifeGame(int board[][MAX_COLS]) {
     int newBoard[MAX_ROWS][MAX_COLS];
-
     for (int i = 0; i < MAX_ROWS; i++) {
         for (int j = 0; j < MAX_COLS; j++) {
             int neighbors = countNeighbors(board, i, j);
-
-            // Apply the rules of the game
             if (board[i][j] == 1 && (neighbors < 2 || neighbors > 3)) {
                 newBoard[i][j] = 0;
             } else if (board[i][j] == 0 && neighbors == 3) {
